a3599fd241e59d1b38c73c8e8e64278ca35a4969
Joerg Fallmann <joerg.fallmann@univie.ac.at>
2014-11-26 16:39:17 +0100

    Updates in supplementer, works for csv, will now delete all xlsx parser capability as it does not work anyway

--- scripts/supplementer.pl
+++ scripts/supplementer.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
 
-#Script supplementer.pl;
-#Last changed Time-stamp: <2014-11-26 14:31:12 fall> by joerg
+### To use this Script replace all semicolons in fields with commas,
+### then all line breaks in fields with nothing,
+### then save as semicolon separated list and have fun parsing
+### 
+### Script supplementer.pl;
+### Last changed Time-stamp: <2014-11-26 16:37:01 fall> by joerg
 ###############
 ###Use stuff
 ###############
@@ -85,79 +89,86 @@ foreach my $file (@csv){
 	open (LIST,"<:gzip(autopop)","$filetoparse");
 	while(<LIST>){
 	    next unless ($_ =~ /.goi./ || $_ =~ /.apk./);
-	    chomp(my $line	  = $_);
+#	    print STDERR $_;
+	    (my $line	  = $_) =~ s/,w+//g;
 	    my @fields		  = split(/\;/,$line);
+#	    print STDERR @fields,"\n";
 	    my $goi		  = $fields[0];
 	    my $hacker		  = $fields[1];
 	    my $gene		  = $fields[2];
 	    my $duplicate	  = $fields[3];   
 	    my @synonyms	  = split(",",$fields[4]);
 	    push @synonyms, $gene unless ($synonyms[0]);
-	    my @pathways	  = split(",",$fields[5]);
-	    my @literature	  = split(",",$fields[6]);
-	    my $igvs		  = $fields[7] || '0';
-	    my $mock		  = $fields[8];
- 	    my $ebola		  = $fields[9];
-	    my $marburg		  = $fields[10];
-	    my $profile		  = $fields[11];
-	    my $homolog		  = $fields[12];
-	    my $raemock		  = $fields[13];
- 	    my $raeebola	  = $fields[14];
-	    my $raemarburg	  = $fields[15];
-	    my $raeprofile	  = $fields[16];
-	    my $ucscs		  = $fields[17] || '0';
-	    my $ucsc_conservation = $fields[18];
-	    my $hg_SNPs		  = $fields[19];
-	    my $rae_SNPs	  = $fields[20];
-	    my $sashimi		  = $fields[21];
-	    my $hg_intron	  = $fields[22];
-	    my $rae_intron	  = $fields[23];
-	    my $hg_5utr		  = $fields[24];
-	    my $hg_3utr		  = $fields[25];
-	    my $extra		  = $fields[26];
-	    my $notes		  = $fields[27];
-	    
-	    $entries{$gene}{GOI}=$goi;
-	    push @{$entries{$gene}{PATHWAY}}, @pathways;
-	    push @{$entries{$gene}{LITERATURE}}, @literature;   
-	    $entries{$gene}{NAME}=$gene;
-	    $entries{$gene}{TEX}="$goi\/$goi\.tex";
-	    if ($igvs == 1){
-		push @{$entries{$gene}{IGV}},"$goi\/snapshots/$goi\_igv.svg";
-	    }
-	    elsif ($igvs == 0){
-		push @{$entries{$gene}{IGV}},"NONE";
-	    }
-	    else{
-		for (1..$igvs){
-		    push @{$entries{$gene}{IGV}},"$goi\/snapshots/$goi\_igv$_\.svg";
+	    if ($duplicate eq '' || $duplicate == 0){
+		my @pathways	      = split(",",$fields[5]) if ($fields[5] ne '');
+		push @pathways, 'Unknown' unless ($pathways[0]);
+		my @literature	      = split(",",$fields[6]) if ($fields[6] ne '');
+		push @literature, 'Unknown' unless ($literature[0]);
+		my $igvs	      = $fields[7] || '0';
+		my $mock	      = $fields[8];
+		my $ebola	      = $fields[9];
+		my $marburg	      = $fields[10];
+		my $profile	      = $fields[11];
+		my $homolog	      = $fields[12];
+		my $raemock	      = $fields[13];
+		my $raeebola	      = $fields[14];
+		my $raemarburg	      = $fields[15];
+		my $raeprofile	      = $fields[16];
+		my $ucscs	      = $fields[17] || '0';
+		my $ucsc_conservation = $fields[18];
+		my $hg_SNPs	      = $fields[19];
+		my $rae_SNPs	      = $fields[20];
+		my $sashimi	      = $fields[21] || '0';
+		my $hg_intron	      = $fields[22];
+		my $rae_intron	      = $fields[23];
+		my $hg_5utr	      = $fields[24];
+		my $hg_3utr	      = $fields[25];
+		my $extra	      = $fields[26];
+		my $notes	      = $fields[27];
+		
+		$entries{$gene}{GOI}  =	$goi;
+		push @{$entries{$gene}{PATHWAY}}, @pathways;
+		push @{$entries{$gene}{LITERATURE}}, @literature;   
+		$entries{$gene}{NAME} =	$gene;
+		$entries{$gene}{TEX}  =	"$goi\/$goi\.tex";
+		if ($igvs == 1){
+		    push @{$entries{$gene}{IGV}},"$goi\/snapshots/$goi\_igv.svg";
 		}
-	    }
-	    if ($ucscs == 1){
-		push @{$entries{$gene}{UCSC}},"$goi\/snapshots/$goi\_ucsc.svg";
-	    }
-	    elsif ($ucscs == 0){
-		push @{$entries{$gene}{UCSC}},"NONE";
-	    }
-	    else{
-		for (1..$ucscs){
-		    push @{$entries{$gene}{UCSC}},"$goi\/snapshots/$goi\_ucsc$_\.svg";
-		}
-	    }
-	    $entries{$gene}{SASHIMI}="$goi\/snapshots/$goi\_sashimi.svg" if ($sashimi > 0);
-	    $entries{$gene}{NOTES}=$notes;
-	    $entries{$gene}{EXTRA}=$extra;
-	    $entries{$gene}{CUFFLINKS}="Filled by Martin";
-	    $entries{$gene}{READS}="Filled by Markus";
-	    
-	    foreach my $syn (@synonyms){
-		if ($duplicate == 1){
-		    $entries{$gene}=$entries{$syn} if ($entries{$syn});
+		elsif ($igvs == 0){
+		    push @{$entries{$gene}{IGV}},"NONE";
 		}
 		else{
-		    $entries{$syn}=$entries{$gene};
+		    for (1..$igvs){
+			push @{$entries{$gene}{IGV}},"$goi\/snapshots/$goi\_igv$_\.svg";
+		    }
 		}
-	    }   
+		if ($ucscs == 1){
+		    push @{$entries{$gene}{UCSC}},"$goi\/snapshots/$goi\_ucsc.svg";
+		}
+		elsif ($ucscs == 0){
+		    push @{$entries{$gene}{UCSC}},"NONE";
+		}
+		else{
+		    for (1..$ucscs){
+			push @{$entries{$gene}{UCSC}},"$goi\/snapshots/$goi\_ucsc$_\.svg";
+		    }
+		}
+		$entries{$gene}{SASHIMI}   = "$goi\/snapshots/$goi\_sashimi.svg" if ($sashimi > 0);
+		$entries{$gene}{NOTES}	   = $notes;
+		$entries{$gene}{EXTRA}	   = $extra;
+		$entries{$gene}{CUFFLINKS} = "Filled by Martin";
+		$entries{$gene}{READS}	   = "Filled by Markus";
+		
+		foreach my $syn (@synonyms){
+		    next if ($syn eq $gene);
+		    $entries{$syn}=$entries{$gene};		    
+		}   
+	    }
+	    else{
+		foreach my $syn (@synonyms){
+		    $entries{$gene}=$entries{$syn} if ($entries{$syn});
+		}
+	    }
 	}
     }
 #    elsif ($filetoparse =~ /.xlsx/){
