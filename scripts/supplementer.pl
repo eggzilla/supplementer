ae2aec32125ca6847ee9a9c14583168b864716bf
Joerg Fallmann <joerg.fallmann@univie.ac.at>
2014-11-28 18:07:16 +0100

    Updates in supplementer.pl, Parsers csvs and creates hash of genes with annotation from tables, can be called via ./supplementer/scripts/supplementer.pl -g Input/GOI.ssv.csv -a Input/APG.ssv.csv -p Input/hg19_mock_ebov.csv

--- scripts/supplementer.pl
+++ scripts/supplementer.pl
@@ -5,7 +5,7 @@
 ### then save as semicolon separated list and have fun parsing
 ### 
 ### Script supplementer.pl;
-### Last changed Time-stamp: <2014-11-26 17:21:58 fall> by joerg
+### Last changed Time-stamp: <2014-11-28 18:05:48 fall> by joerg
 ###############
 ###Use stuff
 ###############
@@ -19,12 +19,6 @@ use File::Path qw(make_path remove_tree);
 use IPC::Cmd qw[can_run run run_forked];
 use PerlIO::gzip;
 use Getopt::Long qw( :config posix_default bundling no_ignore_case );
-use Cwd;
-use Text::Iconv;
- # my $converter = Text::Iconv -> new ("utf-8", "windows-1251");
- # Text::Iconv is not really required.
- # This can be any object with the convert method. Or nothing.
-use Spreadsheet::ParseXLSX;
 use DateTime qw();
 use Data::Dumper;
 
@@ -32,7 +26,7 @@ use Data::Dumper;
 ###Variables
 ###############
 my $VERBOSE = 0;
-my ( $dir, $odir, $files, $outfiles );
+my ( $dir, $odir, $files, $outfiles, $goil, $apgl, $peakl );
 
 ###############
 ###Command Line Options
@@ -40,18 +34,20 @@ my ( $dir, $odir, $files, $outfiles );
 
 pod2usage(-verbose => 0)
     unless GetOptions(
-	"dir|d=s"      => \$dir,
-	"odir|o=s"     => \$odir,
-	"files|f=s"    => \$files,
-	"ofiles|t=s"   => \$outfiles,
-	"help|h"       => sub{pod2usage(-verbose => 1)},
-	"man|m"	       => sub{pod2usage(-verbose => 2)},
-	"verbose"      => sub{ $VERBOSE++ }
+	"dir|d=s"    => \$dir,
+	"odir|o=s"   => \$odir,
+	"goi|g=s"    => \$goil,
+	"apg|a=s"    => \$apgl,
+	"peaks|p=s"  => \$peakl,
+	"ofiles|t=s" => \$outfiles,
+	"help|h"     => sub{pod2usage(-verbose => 1)},
+	"man|m"	     => sub{pod2usage(-verbose => 2)},
+	"verbose"    => sub{ $VERBOSE++ }
     );
 
 $dir  =	 cwd() unless ($dir);
-my @csv = split(/\,/,$files);
 my $today = DateTime->now->strftime('%d%m%Y');
+my @csvs = ($goil,$apgl,$peakl);
 $odir =	 "$dir"."\/Supplements_$today/" unless $odir;
 $dir  =~ s/ //g;
 $odir =~ s/ //g;
@@ -72,23 +68,102 @@ if (!-d $odir){
     make_path ($odir) or die "Error creating directory: $odir";
 }
 
-print STDERR "Reading input!\n";
-
-foreach my $file (@csv){
+my %genes; 
+foreach my $file (@csvs){
+    print STDERR "Reading input!\n";
     die "File $file could not be openend!\n" unless (-e $file);
-    #### Read fields for html from file
-
+#### Read fields for html from file
     my ($wdir,$filetoparse) = split("\/",$file,2);
 #    print STDERR "$wdir, $filetoparse\n";
     chdir ($wdir) or die $!;
-    my %entries; 
+    
+    if ($filetoparse =~ /goi|apg/i ){
+	%genes = %{read_tables($filetoparse,\%genes)};
+    }
+    else{
+	%genes = %{parse_expression($filetoparse,\%genes)};
+    }
+    chdir ($dir) or die $!;
+}
 
-    if ($filetoparse =~ /[.csv|.ssv|.tsv]/ ){
+chdir ($odir) or die $!;
+make_supplements(\%genes);
+
+sub make_supplements{
+    my %gois = %{$_[0]};
+#    foreach my $key( keys %gois ){
+#	print STDERR @{$gois{$key}{PEAKS}} if (defined $gois{$key}{PEAKS});
+#    }
+    print Dumper (\%gois);
+    #check arguments
+#    die ("ERROR $html_destination_path does not exist\n") unless (-d $assembly_hub_destination_path);
+#    die ("ERROR no URL (network location) provided") unless(defined $base_URL);
+#    die ("ERROR $log_path does not exist\n") unless (-e $log_path);
+
+    #ensure that base_URL ends with slash
+#    $base_URL =~ s!/*$!/!;  
+    
+    #check program dependencies
+
+
+    #create html directory structure
+
+    #template definition
+#    my $template = Template->new({
+#	INCLUDE_PATH => ["$template_path"],
+#	RELATIVE=>1,
+#				 });
+
+    #construct index.hmtl
+#    my ($foo, $bar)= (0,0);
+#    my $index_path = $html_destination_path. "/index.html";
+#    my $index_file = 'index.html';
+#    my $index_vars =
+#    {
+#	foo => "$foo",
+#    };
+#    $template->process($index_file,$index_vars,$index_path) || die "Template process failed: ", $template->error(), "\n";
+#
+#    #construct gene of interest goi.html
+#    my $goi_path = $assembly_hub_directory . "/goi.html";
+#    my $goi_file = 'goi.html';
+#    my $goi_vars =
+#    {
+#	bar => "$bar"
+#    };
+#    $template->process($goi_file,$goi_vars,$goi_path) || die "Template process failed: ", $template->error(), "\n";
+}
+
+sub parse_expression{
+#Gene	Mock basemean 3h	Mock basemean 7h	Mock basemean 23h	EBOV basemean 3h	EBOV basemean 7h	Mock basemean 23h	log2(fc) 3h	log2(fc) 7h	log2(fc) 23h	padj(max(fc)	Mock peak 3h	Mock peak 7h	Mock peak 23h	EBOV peak 3h	EBOV peak 7h	EBOV peak 23h
+    my $filetoparse = $_[0];
+    my %entries	    = %{$_[1]};
+    print STDERR "Expression parsing $filetoparse!\n";
+    open (LIST,"<:gzip(autopop)","$filetoparse");
+    while(<LIST>){
+	next if($_ =~ /^#/);
+	my $line  = $_;
+	my ($gene, $mb3, $mb7, $mb23, $eb3, $eb7, $eb23, $l3, $l7, $l23, $max, $mp3, $mp7, $mp23, $ep3, $ep7, $ep23) = split(/\t/,$line);
+#	print STDERR $gene,"\n";
+	push @{$entries{$gene}{CUFFLINKS}}, ($mb3, $mb7, $mb23, $eb3, $eb7, $eb23);
+	push @{$entries{$gene}{LOGEXPRESSION}}, ($l3, $l7, $l23);
+	push @{$entries{$gene}{MAX}}, $max;
+	push @{$entries{$gene}{PEAKS}}, ($mp3, $mp7, $mp23, $ep3, $ep7, $ep23);
+    }
+    return (\%entries);
+}
+
+sub read_tables{
+    my $filetoparse = $_[0];
+    my %entries	    = %{$_[1]};
+    print STDERR "Parsing $filetoparse!\n";
+    open (LIST,"<:gzip(autopop)","$filetoparse");
+    if ($filetoparse =~ /goi/i){
+	print STDERR "Processing GIO List!\n";
+#$entries{$syn}=$entries{$gene};		    
 #hg19.goi.00001;manja;MD2;0;LY96;;;1;00;00;00;0;0-;00;00;00;0;1;11110;0;0;1;0;0;0;0;0;0;#VALUE!;1;1;0;0;1;.;CHECKED
-	print STDERR "Parsing $filetoparse!\n";
-	open (LIST,"<:gzip(autopop)","$filetoparse");
 	while(<LIST>){
-	    next unless ($_ =~ /.goi./ || $_ =~ /.apk./);
+	    next unless ($_ =~ /.goi./);
 #	    print STDERR $_;
 	    (my $line	  = $_) =~ s/,w+//g;
 	    my @fields		  = split(/\;/,$line);
@@ -96,15 +171,18 @@ foreach my $file (@csv){
 	    my $goi		  = $fields[0];
 	    my $hacker		  = $fields[1];
 	    my $gene		  = $fields[2];
+	    next if (defined $entries{$gene});
+
 	    my $duplicate	  = $fields[3];   
 	    my @synonyms	  = split(",",$fields[4]);
 	    push @synonyms, $gene unless ($synonyms[0]);
-	    if ($duplicate eq '' || $duplicate == 0){
+	    if ($duplicate eq '' || $duplicate == 0 || ($duplicate && $fields[8] ne '')){
 		my @pathways	      = split(",",$fields[5]) if ($fields[5] ne '');
 		push @pathways, 'Unknown' unless ($pathways[0]);
 		my @literature	      = split(",",$fields[6]) if ($fields[6] ne '');
 		push @literature, 'Unknown' unless ($literature[0]);
-		my $igvs	      = $fields[7] || '0';
+		my $igvs	      = (split(/[:,\s\/]+/,$fields[7],2))[0] || '0';
+		$igvs = 0 if ($igvs =~ /todo/i);
 		my $mock	      = $fields[8];
 		my $ebola	      = $fields[9];
 		my $marburg	      = $fields[10];
@@ -114,11 +192,13 @@ foreach my $file (@csv){
 		my $raeebola	      = $fields[14];
 		my $raemarburg	      = $fields[15];
 		my $raeprofile	      = $fields[16];
-		my $ucscs	      = $fields[17] || '0';
+		my $ucscs	      = (split(/[:,\s\/]+/,$fields[17],2))[0] || '0';
+		$ucscs = 0 if ($ucscs =~ /todo/i);
 		my $ucsc_conservation = $fields[18];
 		my $hg_SNPs	      = $fields[19];
 		my $rae_SNPs	      = $fields[20];
-		my $sashimi	      = $fields[21] || '0';
+		my $sashimi	      = (split(/[:,\s\/]+/,$fields[21],2))[0] || '0';
+		$sashimi = 0 if ($sashimi =~ /todo/i);
 		my $hg_intron	      = $fields[22];
 		my $rae_intron	      = $fields[23];
 		my $hg_5utr	      = $fields[24];
@@ -156,8 +236,96 @@ foreach my $file (@csv){
 		$entries{$gene}{SASHIMI}   = "$goi\/snapshots/$goi\_sashimi.svg" if ($sashimi > 0);
 		$entries{$gene}{NOTES}	   = $notes;
 		$entries{$gene}{EXTRA}	   = $extra;
-		$entries{$gene}{CUFFLINKS} = "Filled by Martin";
-		$entries{$gene}{READS}	   = "Filled by Markus";
+		
+		foreach my $syn (@synonyms){
+		    next if ($syn eq $gene);
+		    $entries{$syn} = $entries{$gene};		    
+		}   
+	    }
+	    else{
+		foreach my $syn (@synonyms){
+		    next if ($syn eq $gene);
+		    print STDERR "GOI: $gene is a Duplicate of $syn but this has no been processed yet\n" unless ($entries{$syn});
+		    $entries{$gene} = $entries{$syn} if ($entries{$syn});
+		}
+	    }
+	}
+    }
+    elsif ($filetoparse =~ /apg/){
+	print STDERR "Processing APG List!\n";
+	while(<LIST>){
+	    next unless ($_ =~ /.apg./);
+#	    print STDERR $_;
+	    (my $line	  = $_) =~ s/,w+//g;
+	    my @fields		  = split(/\;/,$line);
+#	    print STDERR @fields,"\n";
+	    my $apg		  = $fields[0];
+	    my $hacker		  = $fields[1];
+	    my $gene		  = $fields[2];
+	    my $duplicate	  = $fields[3];   
+	    my @synonyms	  = split(",",$fields[4]);
+	    push @synonyms, $gene unless ($synonyms[0]);
+	    if ($duplicate eq '' || $duplicate == 0){
+		my @pathways	      = split(/[:,]+/,$fields[5]) if ($fields[5] ne '');
+		push @pathways, 'Unknown' unless ($pathways[0]);
+		my @literature	      = split(/[:,]+/,$fields[6]) if ($fields[6] ne '');
+		push @literature, 'Unknown' unless ($literature[0]);
+		my $igvs	      = (split(/[:,\s\/]+/,$fields[7],2))[0] || '0';
+		$igvs = 0 if ($igvs =~ /todo/i);
+		my $scale             = $fields[8] || '0';
+		my $mock	      = $fields[9];
+		my $ebola	      = $fields[10];
+		my $marburg	      = $fields[11];
+		my $profile	      = $fields[12];
+		my $homolog	      = $fields[13];
+		my $raemock	      = $fields[14];
+		my $raeebola	      = $fields[15];
+		my $raemarburg	      = $fields[16];
+		my $raeprofile	      = $fields[17];
+		my $ucscs	      = (split(/[:,\s\/]+/,$fields[17],2))[0] || '0';
+		$ucscs = 0 if ($ucscs =~ /todo/i);
+		my $ucsc_conservation = $fields[19];
+		my $hg_SNPs	      = $fields[20];
+		my $rae_SNPs	      = $fields[21];
+		my $sashimi	      = (split(/[:,\s\/]+/,$fields[21],2))[0] || '0';
+		$sashimi = 0 if ($sashimi =~ /todo/i);
+		my $hg_intron	      = $fields[23];
+		my $rae_intron	      = $fields[24];
+		my $hg_5utr	      = $fields[25];
+		my $hg_3utr	      = $fields[26];
+		my $extra	      = $fields[27];
+		my $notes	      = $fields[28];
+		
+		$entries{$gene}{APG}  =	$apg;
+		push @{$entries{$gene}{PATHWAY}}, @pathways;
+		push @{$entries{$gene}{LITERATURE}}, @literature;   
+		$entries{$gene}{NAME} =	$gene;
+		$entries{$gene}{TEX}  =	"$apg\/$apg\.tex";
+		if ($igvs == 1){
+		    push @{$entries{$gene}{IGV}},"$apg\/snapshots/$apg\_igv.svg";
+		}
+		elsif ($igvs == 0){
+		    push @{$entries{$gene}{IGV}},"NONE";
+		}
+		else{
+		    for (1..$igvs){
+			push @{$entries{$gene}{IGV}},"$apg\/snapshots/$apg\_igv$_\.svg";
+		    }
+		}
+		if ($ucscs == 1){
+		    push @{$entries{$gene}{UCSC}},"$apg\/snapshots/$apg\_ucsc.svg";
+		}
+		elsif ($ucscs == 0){
+		    push @{$entries{$gene}{UCSC}},"NONE";
+		}
+		else{
+		    for (1..$ucscs){
+			push @{$entries{$gene}{UCSC}},"$apg\/snapshots/$apg\_ucsc$_\.svg";
+		    }
+		}
+		$entries{$gene}{SASHIMI}   = "$apg\/snapshots/$apg\_sashimi.svg" if ($sashimi > 0);
+		$entries{$gene}{NOTES}	   = $notes;
+		$entries{$gene}{EXTRA}	   = $extra;
 		
 		foreach my $syn (@synonyms){
 		    next if ($syn eq $gene);
@@ -166,53 +334,27 @@ foreach my $file (@csv){
 	    }
 	    else{
 		foreach my $syn (@synonyms){
+		    next if ($syn eq $gene);
+		    print STDERR "APG: $gene is a Duplicate of $syn but this has no been processed yet\n" unless ($entries{$syn});
 		    $entries{$gene}=$entries{$syn} if ($entries{$syn});
 		}
 	    }
-	}
+	}	
     }
-    
-    chdir ($odir) or die $!;
-    make _supplements(\%entries);
+    return (\%entries);
 }
 
-sub make_supplements{
-    my %gois = %{$_};
-    
-    #check arguments
-    die ("ERROR $html_destination_path does not exist\n") unless (-d $assembly_hub_destination_path);
-    die ("ERROR no URL (network location) provided") unless(defined $base_URL);
-    die ("ERROR $log_path does not exist\n") unless (-e $log_path);
+sub unique_array{
 
-    #ensure that base_URL ends with slash
-    $base_URL =~ s!/*$!/!;  
-	 
-  #check program dependencies
+    my $arrayref = shift;
+    my @array = @{$arrayref};
 
-
-  #create html directory structure
-
-  #template definition
-  my $template = Template->new({
-                                INCLUDE_PATH => ["$template_path"],
-                                RELATIVE=>1,
-  });
-
-  #construct index.hmtl
-  my $index_path = $html_destination_path. "/index.html";
-  my $index_file = 'index.html';
-  my $index_vars =
+    my %unique = ();
+    foreach my $item (@array)
     {
-     foo => "$foo",
-    };
-  $template->process($index_file,$index_vars,$index_path) || die "Template process failed: ", $template->error(), "\n";
+        $unique{$item} ++;
+    }
+    my @arrayuid = sort {$a cmp $b} keys %unique;
 
-  #construct gene of interest goi.html
-  my $goi_path = $assembly_hub_directory . "/goi.html";
-  my $goi_file = 'goi.html';
-  my $goi_vars =
-    {
-     bar => "$bar"
-    };
-  $template->process($goi_file,$goi_vars,$goi_path) || die "Template process failed: ", $template->error(), "\n";
+    return(\@arrayuid);
 }
